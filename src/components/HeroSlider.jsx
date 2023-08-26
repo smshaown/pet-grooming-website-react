70a2b6fcaab83528aad81c386e8bf481bf507726
smshaown <smshaown65@gmail.com>
2023-08-26 13:53:29 +0600

    swper silder creact start

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -1,9 +1,16 @@
+import {Swiper, SwiperSlide} from 'swiper/react'
 
+
+// import swiper styles 
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import '../swiper.css'
 
 const HeroSlider = () => {
   return (
     <div>
-      
+      Hero Slider
     </div>
   ); 
 };
